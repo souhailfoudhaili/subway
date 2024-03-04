a35ff2fc3a452e372b35bf96cd525ac106007d27
souhailfoudhaili <souhail.foudhaili@esprit.tn>
2024-03-04 19:41:34 +0100

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    int score;
+    public static GameManager inst;
+
+    [SerializeField] Text scoreText;
+
+
+    public void IncrementScore ()
+    {
+        score++;
+        scoreText.text = "SCORE: " + score;
+
+    }
+
+    private void Awake ()
+    {
+        inst = this;
+    }
+
+    private void Start () {
+
+	}
+
+	private void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour {
+
+    bool alive = true;
+
+    public float speed = 5;
+    [SerializeField] Rigidbody rb;
+
+    float horizontalInput;
+
+    // Variables for touch input
+    bool isDragging = false;
+    Vector2 dragStartPosition;
+
+    private void FixedUpdate ()
+    {
+        if (!alive) return;
+
+        // Calculate movement based on touch input or keyboard input
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+
+    private void Update () {
+        // Handle touch input
+        HandleTouchInput();
+
+        // Check for falling off the platform
+        if (transform.position.y < -5) {
+            Die();
+        }
+	}
+
+    void HandleTouchInput()
+{
+    // Handle touch input to move the player horizontally
+    if (Input.touchCount > 0) {
+        Touch touch = Input.GetTouch(0);
+        switch (touch.phase) {
+            case TouchPhase.Began:
+                isDragging = true;
+                dragStartPosition = touch.position;
+                break;
+            case TouchPhase.Moved:
+                if (isDragging) {
+                    horizontalInput = (touch.position.x - dragStartPosition.x) / Screen.width * 5f; 
+                }
+                break;
+            case TouchPhase.Ended:
+                isDragging = false;
+                horizontalInput = 0f;
+                break;
+        }
+    }
+}
+
+
+    public void Die ()
+    {
+        alive = false;
+        // Restart the game
+        Invoke("Restart", 2);
+    }
+
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
